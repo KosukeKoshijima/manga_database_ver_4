6c49c907059cc0d452d171fe3c1d12a0679bddae
Kosuke Koshijima <kosuke.koshijima@gmail.com>
2017-06-08 18:40:45 -0500

    6

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -6,5 +6,10 @@ class Manga < ApplicationRecord
   has_many :fans, :through => :likes, :source => :user
 
   validates :user_id, :presence => true
-
+validates :title, :presence => true
+validates :image, :presence => true
+validates :year, :presence => true
+validates :author, :presence => true
+validates :character, :presence => true
+validates :magazine, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,4 @@ class User < ApplicationRecord
 
 
 
-
-
 end
